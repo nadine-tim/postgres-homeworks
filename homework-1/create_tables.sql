a62672665b18559accb52e81314d8c4b198cdf0a
nadine-tim <nadinetimeiko@gmail.com>
2023-06-07 22:26:45 +0300

    hw-1 done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+create table employees
+(
+	employee_id serial primary key,
+	first_name varchar(20),
+	last_name varchar(30),
+	title varchar(255),
+	birth_date date,
+	notes text
+);
+
+create table customers
+(
+	customer_id varchar(10) primary key,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+create table orders
+(
+	order_id int primary key,
+	customer_id varchar(10) references customers(customer_id),
+	employee_id int references employees(employee_id),
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,32 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+def connection_to_db():
+    """
+    Подключение и запись в БД
+    """
+
+    path = ["north_data/customers_data.csv", "north_data/employees_data.csv", "north_data/orders_data.csv"]
+    query = ["insert into customers values (%s, %s, %s)",
+             "insert into employees values (%s, %s, %s, %s, %s, %s)",
+             "insert into orders values (%s, %s, %s, %s, %s)"]
+
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='pgadmin')
+    cur = conn.cursor()
+
+    try:
+        for i in range(len(path)):
+            with open(path[i], 'r') as f:
+                reader = csv.reader(f)
+                next(reader)
+                for row in reader:
+                    cur.execute(query[i], row)
+            conn.commit()
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    connection_to_db()
